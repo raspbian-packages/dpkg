2d70730151f588bfc2dea4a79aa7dc7aa057ca2f
Guillem Jover <guillem@debian.org>
2016-07-31 10:57:02 +0000

    dpkg (1.18.10) unstable; urgency=medium
      [ Guillem Jover ]
      * Fix a short-lived memory leak in dpkg archive argument parsing.
      * When activating file triggers on conffile purge, use the conffile name
        instead of the real pathname it might refer to. This fixes a segfault
        when using --instdir, or when the conffile has been moved around and
        replaced with a symlink to the target.
        Regression introduced in dpkg 1.18.8. Closes: #830267
      * Perl modules:
        - Disable fixdebugpath feature on unsafe characters in the path.
        - Generate reproducible source tarballs by using the new GNU tar
          --clamp-mtime option in Dpkg::Source::Archive, to make sure no file
          in source packages has an mtime later than the changelog entry time.
        - Enable fixdebugpath build flag feature by default.
          Thanks to Mattia Rizzolo <mattia@debian.org>. Closes: #832179
        - Rename various private methods and functions with an underscore prefix
          and unified names.
        - Refactor perl modules directory list into a new function in Test::Dpkg.
      * Test suite:
        - Add new POD coverage author test for public perl modules.
      * Documentation:
        - Document Testsuite-Triggers in dsc(5).
        - Fix deb-changes(5) description to talk about .changes instead of .dsc.
          Reported by HW42 <hw42@ipsumj.de>.
        - Use debian/control as the SYNOPSIS in deb-src-control(5).
        - Add new deb-conffiles(5) man page.
        - Add references to deb-control(5) and deb-triggers(5) in dpkg(1).
        - Fix typos for repeated “the” word.
          Prompted by Valentin Samir <valentin.samir@crans.org> in #830989.
        - Fix misspelled -fdebug-prefix-map flag in dpkg-buildflags(1).
          Reported by Mattia Rizzolo <mattia@debian.org>.
        - Remove mention of source format 1.0 supporting building with upstream
          tarball signatures from dpkg-source(1), as this was disabled in 1.18.8.
        - Document all public methods in Dpkg::Changelog::Entry::Debian.
        - Escape dashes in deb-changes(5) and dsc(5) man pages.
        - Fix typos in dpkg-shlibdeps(1). Closes: #832434
          Thanks to Carsten Leonhardt <leo@debian.org>.
    
      [ Updated programs translations ]
      * German (Sven Joachim).
      * Turkish (Mert Dirik). Closes: #832070
    
      [ Updated scripts translations ]
      * German (Helge Kreutzmann).
    
      [ Updated manpages translations ]
      * German (Helge Kreutzmann).
    
    [dgit import package dpkg 1.18.10]

---
+++ scripts/Test/Dpkg.pm
@@ -0,0 +1,104 @@
+# Copyright © 2015 Guillem Jover <guillem@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package Test::Dpkg;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.00';
+our @EXPORT_OK = qw(
+    all_perl_files
+    test_get_perl_dirs
+    test_needs_author
+    test_needs_module
+    test_needs_command
+    test_needs_srcdir_switch
+);
+our %EXPORT_TAGS = (
+    needs => [ qw(
+        test_needs_author
+        test_needs_module
+        test_needs_command
+        test_needs_srcdir_switch
+    ) ],
+);
+
+use Exporter qw(import);
+use File::Find;
+use IPC::Cmd qw(can_run);
+use Test::More;
+
+sub test_get_perl_dirs
+{
+    return qw(t src/t lib utils/t scripts dselect);
+}
+
+sub all_perl_files
+{
+    my @files;
+    my $scan_perl_files = sub {
+        push @files, $File::Find::name if m/\.(pl|pm|t)$/;
+    };
+
+    find($scan_perl_files, test_get_perl_dirs());
+
+    return @files;
+}
+
+sub test_needs_author
+{
+    if (not $ENV{DPKG_DEVEL_MODE} and not $ENV{AUTHOR_TESTING}) {
+        plan skip_all => 'developer test';
+    }
+}
+
+sub test_needs_module
+{
+    my ($module, @imports) = @_;
+    my ($package) = caller;
+
+    require version;
+    my $version = '';
+    if (@imports >= 1 and version::is_lax($imports[0])) {
+        $version = shift @imports;
+    }
+
+    eval qq{
+        package $package;
+        use $module $version \@imports;
+        1;
+    } or do {
+        plan skip_all => "requires module $module $version";
+    }
+}
+
+sub test_needs_command
+{
+    my $command = shift;
+
+    if (not can_run($command)) {
+        plan skip_all => "requires command $command";
+    }
+}
+
+sub test_needs_srcdir_switch
+{
+    if (defined $ENV{srcdir}) {
+        chdir $ENV{srcdir} or BAIL_OUT("cannot chdir to source directory: $!");
+    }
+}
+
+1;
