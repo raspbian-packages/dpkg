6fccd71025a777ebf4606059d865d7a1c91c5383
Guillem Jover <guillem@debian.org>
2024-07-24 23:02:45 +0200

    dpkg (1.22.9) unstable; urgency=medium
      [ Guillem Jover ]
      * dpkg-deb: Add long options for short-only compression ones.
      * dpkg-deb: Print pathname instead of filename and directory in error output.
      * start-stop-daemon: Use a stop context to track the current state.
      * Make fragments:
        - Fix pkg-info.mk evaluation by adding new DEB_TIMESTAMP variable.
          Closes: #1076863
      * Code internals:
        - dpkg-deb: Track maintscript information in a new struct.
      * Test suite:
        - Fix typos in Makefile comments.
    
    [dgit import package dpkg 1.22.9]

---
+++ scripts/mk/pkg-info.mk
@@ -0,0 +1,40 @@
+# This Makefile fragment (since dpkg 1.16.1) defines the following package
+# information variables:
+#
+#   DEB_SOURCE: source package name.
+#   DEB_VERSION: package's full version (epoch + upstream vers. + revision).
+#   DEB_VERSION_EPOCH_UPSTREAM: package's version without the Debian revision.
+#   DEB_VERSION_UPSTREAM_REVISION: package's version without the Debian epoch.
+#   DEB_VERSION_UPSTREAM: package's upstream version.
+#   DEB_DISTRIBUTION: distribution(s) listed in the current debian/changelog
+#     entry.
+#   DEB_TIMESTAMP: source package release date as seconds since the epoch as
+#     specified in the latest debian/changelog entry (since dpkg 1.22.9),
+#     although you are probably looking for SOURCE_DATE_EPOCH instead.
+#
+#   SOURCE_DATE_EPOCH: source release date as seconds since the epoch, as
+#     specified by <https://reproducible-builds.org/specs/source-date-epoch/>
+#     (since dpkg 1.18.8).
+#     If it is undefined, the date of the latest changelog entry is used.
+#     In both cases, the value is exported.
+
+ifndef dpkg_pkg_info_mk_included
+dpkg_pkg_info_mk_included = yes
+
+dpkg_parsechangelog_run = $(eval $(shell dpkg-parsechangelog | sed -n '\
+  s/^Distribution: \(.*\)/$$(eval DEB_DISTRIBUTION:=\1)/p;\
+  s/^Source: \(.*\)/$$(eval DEB_SOURCE:=\1)/p;\
+  s/^Version: \([0-9]*:\)\{0,1\}\([^-]*\)\(\(.*\)-[^-]*\)\{0,1\}$$/\
+    $$(eval DEB_VERSION:=\1\2\3)\
+    $$(eval DEB_VERSION_EPOCH_UPSTREAM:=\1\2\4)\
+    $$(eval DEB_VERSION_UPSTREAM_REVISION:=\2\3)\
+    $$(eval DEB_VERSION_UPSTREAM:=\2\4)/p;\
+  s/^Timestamp: \(.*\)/$$(eval DEB_TIMESTAMP:=\1)/p'))
+
+# Compute all the values in one go.
+$(dpkg_parsechangelog_run)
+
+SOURCE_DATE_EPOCH ?= $(DEB_TIMESTAMP)
+export SOURCE_DATE_EPOCH
+
+endif # dpkg_pkg_info_mk_included
---
+++ scripts/mk/vendor.mk
@@ -0,0 +1,58 @@
+# This Makefile fragment (since dpkg 1.16.1) defines the following
+# vendor-related variables:
+#
+#   DEB_VENDOR: output of «dpkg-vendor --query Vendor».
+#   DEB_PARENT_VENDOR: output of «dpkg-vendor --query Parent» (can be empty).
+#
+# This Makefile fragment also defines a set of "dpkg_vendor_derives_from"
+# macros that can be used to verify if the current vendor derives from
+# another vendor. The unversioned variant defaults to the v0 version if
+# undefined or to the v1 version when on build API 1, otherwise it can be
+# defined explicitly to one of the versions or the versioned macros can be
+# used directly. The following are example usages:
+#
+# - dpkg_vendor_derives_from (since dpkg 1.16.1)
+#
+#   ifeq ($(shell $(call dpkg_vendor_derives_from,ubuntu)),yes)
+#     ...
+#   endif
+#
+# - dpkg_vendor_derives_from_v0 (since dpkg 1.19.3)
+#
+#   ifeq ($(shell $(call dpkg_vendor_derives_from_v0,ubuntu)),yes)
+#     ...
+#   endif
+#
+# - dpkg_vendor_derives_from_v1 (since dpkg 1.19.3)
+#
+#   dpkg_vendor_derives_from = $(dpkg_vendor_derives_from_v1)
+#   ifeq ($(call dpkg_vendor_derives_from,ubuntu),yes)
+#     ...
+#   endif
+#   ifeq ($(call dpkg_vendor_derives_from_v1,ubuntu),yes)
+#     ...
+#   endif
+
+ifndef dpkg_vendor_mk_included
+dpkg_vendor_mk_included = yes
+
+ifndef dpkg_datadir
+  dpkg_datadir := $(dir $(lastword $(MAKEFILE_LIST)))
+endif
+include $(dpkg_datadir)/buildapi.mk
+
+dpkg_lazy_eval ?= $(eval $(1) = $(2)$$($(1)))
+dpkg_lazy_set ?= $(call dpkg_lazy_eval,$(1),$$(eval $(1) := $(2)))
+$(call dpkg_lazy_set,DEB_VENDOR,$$(shell dpkg-vendor --query Vendor))
+$(call dpkg_lazy_set,DEB_PARENT_VENDOR,$$(shell dpkg-vendor --query Parent))
+
+dpkg_vendor_derives_from_v0 = dpkg-vendor --derives-from $(1) && echo yes || echo no
+dpkg_vendor_derives_from_v1 = $(shell $(dpkg_vendor_derives_from_v0))
+
+ifeq ($(call dpkg_build_api_ge,1),yes)
+dpkg_vendor_derives_from ?= $(dpkg_vendor_derives_from_v1)
+else
+dpkg_vendor_derives_from ?= $(dpkg_vendor_derives_from_v0)
+endif
+
+endif # dpkg_vendor_mk_included
