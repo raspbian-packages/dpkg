900099b9bfc013ddc3a4905bc673eba41c7fd019
Guillem Jover <guillem@debian.org>
2024-08-01 12:39:16 +0200

    dpkg (1.22.11) unstable; urgency=medium
      [ Guillem Jover ]
      * Perl modules:
        - Dpkg::Shlibs::Cppfilt: Do not normalize angle brackets for operators.
          Closes: #1076067
      * Documentation:
        - doc: Replace references to crypto-refresh and RFC4880 with RFC9580.
      * Build system:
        - Use 'command -v' instead of 'which'.
        - Remove generated spacesyms.o and spacesyms.c on clean.
        - Add declaration for funcs symbols in spacesyms.c.
      * Test suite:
        - Fix coding style in perl test cases.
        - Improve Dpkg::Changelog data test case message.
        - Refactor setup_changelog() in Dpkg::Changelog test.
        - Unroll Dpkg::Changelog file tests loop.
        - Refactor load_objdump_obj().
    
      [ Helge Kreutzmann ]
      * Localization:
        - Update German man pages translation.
        - Update German scripts translation.
    
      [ Sven Joachim ]
      * Localization:
        - Update German programs translation.
    
    [dgit import package dpkg 1.22.11]

---
+++ scripts/mk/default.mk
@@ -0,0 +1,19 @@
+# This Makefile fragment (since dpkg 1.16.1) includes all the Makefile
+# fragments that define variables that can be useful within debian/rules.
+
+ifndef dpkg_default_mk_included
+dpkg_default_mk_included = yes
+
+dpkg_datadir := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(dpkg_datadir)/architecture.mk
+include $(dpkg_datadir)/buildapi.mk
+ifeq ($(call dpkg_build_api_ge,1),yes)
+include $(dpkg_datadir)/buildtools.mk
+endif
+include $(dpkg_datadir)/buildflags.mk
+include $(dpkg_datadir)/buildopts.mk
+include $(dpkg_datadir)/pkg-info.mk
+include $(dpkg_datadir)/vendor.mk
+
+endif # dpkg_default_mk_included
